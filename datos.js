d3cf1468a1d36d195e804d4050a50e99d3ac51af
Dory302 <co.sepulvedad@duocuc.cl>
2026-09-08 13:58:48 -0300

    Update datos.js
    Cambios de productos , precios , imagenes , etc

--- datos.js
+++ datos.js
@@ -10,19 +10,19 @@ const productosIniciales = [
     precio: 29990,
     stock: 10,
     stockCritico: 2,
-    descripcion: "Polerón de corte amplio con capucha ajustable e interior suave para un look urbano cómodo.",
+    descripcion: "Polerón de corte amplio con capucha e interior suave para un look urbano cómodo.",//Se le saco lo de ajustable ya que era de mientras el texto y decidi colocar un poleron mas normal pero oversize
     material: "Algodón 100%",
-    img: "https://images.unsplash.com/photo-1556905055-8f358a7a47b2?w=500&auto=format&fit=crop&q=60"
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSSTt2Y6t0-9FY60nDY8MUmk_99Tbw8pxxR1MTjNCfQcg&s=10" //Se cambio la imagen del producto ya que la que estaba era una de prueba generica//
   },
   {
     id: "URB002",
-    nombre: "Pantalón Cargo Black",
+    nombre: "Pantalón Oversize",//En vez de un pantalon cargo opte por uno oversize ya que asi se hace el conjunto con el poleron y lo compren
     precio: 34990,
     stock: 8,
     stockCritico: 3,
-    descripcion: "Pantalón cargo de tiro medio con bolsillos funcionales y silueta relajada para el uso diario.",
+    descripcion: "Pantalón Oversize de tiro medio con bolsillos funcionales y silueta relajada para el uso diario.",
     material: "Sarga de algodón",
-    img: "https://images.unsplash.com/photo-1624378439575-d8705ad7ae80?w=500&auto=format&fit=crop&q=60"
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQRMkgRXffxVzlFd3ditJA-mVDHek44FfC7dpLMm5YtrQ&s=10"//Se cambia la imagen del producto ya que era una de prueba la anterior
   },
   {
     id: "URB003",
@@ -36,52 +36,52 @@ const productosIniciales = [
   },
   {
     id: "URB004",
-    nombre: "Polera Graphic Street",
+    nombre: "Polera Urbana",
     precio: 18990,
     stock: 20,
     stockCritico: 5,
     descripcion: "Polera de calce regular con gráfico frontal y tejido ligero para combinar en cualquier temporada.",
     material: "Algodón 100%",
-    img: "https://images.unsplash.com/photo-1521572267360-ee0c2909d518?w=500&auto=format&fit=crop&q=60"
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRopC8QhHmsTrmZipIQ8_hIlp6O4luUYKxwMOM-2OFhzA&s=10"//Se cambia la imagen del producto ya que era una de prueba la anterior
   },
   {
     id: "URB005",
-    nombre: "Zapatillas Street Red",
+    nombre: "Zapatillas Urbanas Skaters",
     precio: 59990,
     stock: 7,
     stockCritico: 2,
-    descripcion: "Zapatillas de perfil bajo con detalle rojo, amortiguación ligera y una suela pensada para la ciudad.",
+    descripcion: "Zapatillas de perfil bajo, amortiguación ligera y una suela pensada para andar en skate.",//Se decidio cambiar el producto al final 
     material: "Textil y goma",
-    img: "https://images.unsplash.com/photo-1542291026-7eec264c27ff?w=500&auto=format&fit=crop&q=60"
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS4YPJ325TLXnJshKUzI1R_owl9_Gn5js0KXpuTnkcOuQ&s=10"//Se cambia la imagen del producto ya que era una de prueba la anterior
   },
   {
     id: "URB006",
-    nombre: "Chaqueta Denim Street",
+    nombre: "Chaqueta Mezclilla Urbana",
     precio: 42990,
     stock: 6,
     stockCritico: 2,
     descripcion: "Chaqueta de mezclilla con estructura firme, bolsillos frontales y terminación urbana clásica.",
     material: "Denim de algodón",
-    img: "https://images.unsplash.com/photo-1576995853123-5a10305d93c0?w=500&auto=format&fit=crop&q=60"
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQQjaaIVgiz3MelEdalK9RNi_qHVWp2tJ2HMdDEvNtgyw&s=10"//Se cambia la imagen del producto ya que era una de prueba la anterior
   },
   {
     id: "URB007",
-    nombre: "Jockey Snapback",
-    precio: 14990,
+    nombre: "Conjunto Urbano",
+    precio: 30990,//Se cambia el precio del producto ya que el producto cambio y ahora es un conjunto
     stock: 16,
     stockCritico: 4,
-    descripcion: "Jockey ajustable de seis paneles con visera curva y cierre posterior para un calce personalizado.",
+    descripcion: "Conjunto Urbano negro , como y especial para usar en el dia.",
     material: "Algodón y poliéster",
-    img: "https://images.unsplash.com/photo-1588850561407-ed78c282e89b?w=500&auto=format&fit=crop&q=60"
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRt1LSl0-vmtALD_tVZx4Q34POh3uIlDaz8GRO7EZydRQ&s=10"//Se cambia la imagen del producto ya que era una de prueba la anterior
   },
   {
     id: "URB008",
-    nombre: "Polerón Zip Hoodie",
+    nombre: "Polerón Oversize",
     precio: 32990,
     stock: 9,
     stockCritico: 3,
-    descripcion: "Polerón con cierre completo, capucha y bolsillos delanteros para capas fáciles durante el día.",
-    material: "Algodón afelpado",
-    img: "https://images.unsplash.com/photo-1608256246200-53e635b5b65f?w=500&auto=format&fit=crop&q=60"
+    descripcion: "Polerón urbano, con capucha y comodo para tu uso del dia a dia .",
+    material: "Algodón ",
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSEgGoHoynyw-Si5maxr58UaCpAm3fNne1RKim52mAnXA&s=10"//Se cambia la imagen del producto ya que era una de prueba la anterior
   }
 ];
